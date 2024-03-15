5f03b703f2e994d729a36cf6c4caa49c99338d18
frederik <frederik.slos@gmail.com>
2024-03-15 10:33:57 +0100

    convert views.php

--- homepage/views.php
+++ homepage/views.php
@@ -6,27 +6,13 @@ $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
 session_start();
 
-if(!isset($_SESSION['my_timezone'])) {
-  $_SESSION['my_timezone'] = trim(shell_exec('timedatectl show --value --property=Timezone'));
-}
-date_default_timezone_set($_SESSION['my_timezone']);
+require_once 'scripts/common.php';
+$user = get_user();
+$home = get_home();
+$config = get_config();
+set_timezone();
 
-$user = shell_exec("awk -F: '/1000/{print $1}' /etc/passwd");
-$user = trim($user);
-$home = shell_exec("awk -F: '/1000/{print $6}' /etc/passwd");
-$home = trim($home);
-if (file_exists('./scripts/thisrun.txt')) {
-  $config = parse_ini_file('./scripts/thisrun.txt');
-} elseif (file_exists('./scripts/firstrun.ini')) {
-  $config = parse_ini_file('./scripts/firstrun.ini');
-}
-function service_mount() {
-  global $home;
-  $service_mount=trim(shell_exec("systemd-escape -p --suffix=mount ".$home."/BirdSongs/StreamData"));
-  return $service_mount;
-}
-$authenticated = isset($_SERVER['PHP_AUTH_USER']) && $_SERVER['PHP_AUTH_PW'] == $config['CADDY_PWD'] && $_SERVER['PHP_AUTH_USER'] == 'birdnet';
-if($authenticated && (!isset($_SESSION['behind']) || !isset($_SESSION['behind_time']) || time() > $_SESSION['behind_time'] + 86400)) {
+if(is_authenticated() && (!isset($_SESSION['behind']) || !isset($_SESSION['behind_time']) || time() > $_SESSION['behind_time'] + 86400)) {
   shell_exec("sudo -u".$user." git -C ".$home."/BirdNET-Pi fetch > /dev/null 2>/dev/null &");
   $str = trim(shell_exec("sudo -u".$user." git -C ".$home."/BirdNET-Pi status"));
   if (preg_match("/behind '.*?' by (\d+) commit(s?)\b/", $str, $matches)) {
@@ -135,8 +121,14 @@ function copyOutput(elem) {
 <?php
 if(isset($_GET['view'])){
   if($_GET['view'] == "System Info"){echo "<iframe src='phpsysinfo/index.php'></iframe>";}
-  if($_GET['view'] == "System Controls"){include('scripts/system_controls.php');}
-  if($_GET['view'] == "Services"){include('scripts/service_controls.php');}
+  if($_GET['view'] == "System Controls"){
+    ensure_authenticated();
+    include('scripts/system_controls.php');
+  }
+  if($_GET['view'] == "Services"){
+    ensure_authenticated();
+    include('scripts/service_controls.php');
+  }
   if($_GET['view'] == "Spectrogram"){include('spectrogram.php');}
   if($_GET['view'] == "View Log"){echo "<body style=\"scroll:no;overflow-x:hidden;\"><iframe style=\"width:calc( 100% + 1em);\" src=\"/log\"></iframe></body>";}
   if($_GET['view'] == "Overview"){include('overview.php');}
@@ -147,42 +139,27 @@ if(isset($_GET['view'])){
   if($_GET['view'] == "Streamlit"){echo "<iframe src=\"/stats\"></iframe>";}
   if($_GET['view'] == "Daily Charts"){include('history.php');}
   if($_GET['view'] == "Tools"){
-    $caddypwd = $config['CADDY_PWD'];
-    if (!isset($_SERVER['PHP_AUTH_USER'])) {
-      header('WWW-Authenticate: Basic realm="My Realm"');
-      header('HTTP/1.0 401 Unauthorized');
-      echo '<table><tr><td>You cannot edit the settings for this installation</td></tr></table>';
-      exit;
-    } else {
-      $submittedpwd = $_SERVER['PHP_AUTH_PW'];
-      $submitteduser = $_SERVER['PHP_AUTH_USER'];
-      if($submittedpwd == $caddypwd && $submitteduser == 'birdnet'){
-        $url = $_SERVER['SERVER_NAME']."/scripts/adminer.php";
-        echo "<div class=\"centered\">
-          <form action=\"\" method=\"GET\" id=\"views\">
-          <button type=\"submit\" name=\"view\" value=\"Settings\" form=\"views\">Settings</button>
-          <button type=\"submit\" name=\"view\" value=\"System Info\" form=\"views\">System Info</button>
-          <button type=\"submit\" name=\"view\" value=\"System Controls\" form=\"views\">System Controls".$updatediv."</button>
-          <button type=\"submit\" name=\"view\" value=\"Services\" form=\"views\">Services</button>
-          <button type=\"submit\" name=\"view\" value=\"File\" form=\"views\">File Manager</button>
-          <a href=\"scripts/adminer.php\" target=\"_blank\"><button type=\"submit\" form=\"\">Database Maintenance</button></a>
-          <button type=\"submit\" name=\"view\" value=\"Webterm\" form=\"views\">Web Terminal</button>
-          <button type=\"submit\" name=\"view\" value=\"Included\" form=\"views\">Custom Species List</button>
-          <button type=\"submit\" name=\"view\" value=\"Excluded\" form=\"views\">Excluded Species List</button>
-          </form>
-          </div>";
-      } else {
-        header('WWW-Authenticate: Basic realm="My Realm"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo '<table><tr><td>You cannot edit the settings for this installation</td></tr></table>';
-        exit;
-      }
-    }
+    ensure_authenticated();
+    $url = $_SERVER['SERVER_NAME']."/scripts/adminer.php";
+    echo "<div class=\"centered\">
+      <form action=\"\" method=\"GET\" id=\"views\">
+      <button type=\"submit\" name=\"view\" value=\"Settings\" form=\"views\">Settings</button>
+      <button type=\"submit\" name=\"view\" value=\"System Info\" form=\"views\">System Info</button>
+      <button type=\"submit\" name=\"view\" value=\"System Controls\" form=\"views\">System Controls".$updatediv."</button>
+      <button type=\"submit\" name=\"view\" value=\"Services\" form=\"views\">Services</button>
+      <button type=\"submit\" name=\"view\" value=\"File\" form=\"views\">File Manager</button>
+      <a href=\"scripts/adminer.php\" target=\"_blank\"><button type=\"submit\" form=\"\">Database Maintenance</button></a>
+      <button type=\"submit\" name=\"view\" value=\"Webterm\" form=\"views\">Web Terminal</button>
+      <button type=\"submit\" name=\"view\" value=\"Included\" form=\"views\">Custom Species List</button>
+      <button type=\"submit\" name=\"view\" value=\"Excluded\" form=\"views\">Excluded Species List</button>
+      </form>
+      </div>";
   }
   if($_GET['view'] == "Recordings"){include('play.php');}
   if($_GET['view'] == "Settings"){include('scripts/config.php');} 
   if($_GET['view'] == "Advanced"){include('scripts/advanced.php');}
   if($_GET['view'] == "Included"){
+    ensure_authenticated();
     if(isset($_GET['species']) && isset($_GET['add'])){
       $file = './scripts/include_species_list.txt';
       $str = file_get_contents("$file");
@@ -211,6 +188,7 @@ if(isset($_GET['view'])){
     include('./scripts/include_list.php');
   }
   if($_GET['view'] == "Excluded"){
+    ensure_authenticated();
     if(isset($_GET['species']) && isset($_GET['add'])){
       $file = './scripts/exclude_species_list.txt';
       $str = file_get_contents("$file");
@@ -240,88 +218,53 @@ if(isset($_GET['view'])){
     echo "<iframe src='scripts/filemanager/filemanager.php'></iframe>";
   }
   if($_GET['view'] == "Webterm"){
-    if (file_exists('./scripts/thisrun.txt')) {
-      $config = parse_ini_file('./scripts/thisrun.txt');
-    } elseif (file_exists('./scripts/firstrun.ini')) {
-      $config = parse_ini_file('./scripts/firstrun.ini');
-    }
-    $caddypwd = $config['CADDY_PWD'];
-    if (!isset($_SERVER['PHP_AUTH_USER'])) {
-      header('WWW-Authenticate: Basic realm="My Realm"');
-      header('HTTP/1.0 401 Unauthorized');
-      echo '<table><tr><td>You cannot access the web terminal</td></tr></table>';
-      exit;
-    } else {
-      $submittedpwd = $_SERVER['PHP_AUTH_PW'];
-      $submitteduser = $_SERVER['PHP_AUTH_USER'];
-      if($submittedpwd == $caddypwd && $submitteduser == 'birdnet'){
-        #ACCESS THE WEB TERMINAL
-        echo "<iframe src='/terminal'></iframe>";
-      } else {
-        header('WWW-Authenticate: Basic realm="My Realm"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo '<table><tr><td>You cannot access the web terminal</td></tr></table>';
-        exit;
-      }
-    }
+    ensure_authenticated('You cannot access the web terminal');
+    echo "<iframe src='/terminal'></iframe>";
   }
 } elseif(isset($_GET['submit'])) {
-  if (file_exists('./scripts/thisrun.txt')) {
-    $config = parse_ini_file('./scripts/thisrun.txt');
-  } elseif (file_exists('./scripts/firstrun.ini')) {
-    $config = parse_ini_file('./scripts/firstrun.ini');
-  }
-  $caddypwd = $config['CADDY_PWD'];
-  if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'You cannot access the web terminal';
-    exit;
-  } else {
-    $submittedpwd = $_SERVER['PHP_AUTH_PW'];
-    $submitteduser = $_SERVER['PHP_AUTH_USER'];
-    $allowedCommands = array('sudo systemctl stop livestream.service && sudo systemctl stop icecast2.service',
-                       'sudo systemctl restart livestream.service && sudo systemctl restart icecast2.service',
-                       'sudo systemctl disable --now livestream.service && sudo systemctl disable icecast2 && sudo systemctl stop icecast2.service',
-                       'sudo systemctl enable icecast2 && sudo systemctl start icecast2.service && sudo systemctl enable --now livestream.service',
-                       'sudo systemctl stop web_terminal.service',
-                       'sudo systemctl restart web_terminal.service',
-                       'sudo systemctl disable --now web_terminal.service',
-                       'sudo systemctl enable --now web_terminal.service',
-                       'sudo systemctl stop birdnet_log.service',
-                       'sudo systemctl restart birdnet_log.service',
-                       'sudo systemctl disable --now birdnet_log.service',
-                       'sudo systemctl enable --now birdnet_log.service',
-                       'sudo systemctl stop birdnet_analysis.service',
-                       'sudo systemctl restart birdnet_analysis.service',
-                       'sudo systemctl disable --now birdnet_analysis.service',
-                       'sudo systemctl enable --now birdnet_analysis.service',
-                       'sudo systemctl stop birdnet_stats.service',
-                       'sudo systemctl restart birdnet_stats.service',
-                       'sudo systemctl disable --now birdnet_stats.service',
-                       'sudo systemctl enable --now birdnet_stats.service',
-                       'sudo systemctl stop birdnet_recording.service',
-                       'sudo systemctl restart birdnet_recording.service',
-                       'sudo systemctl disable --now birdnet_recording.service',
-                       'sudo systemctl enable --now birdnet_recording.service',
-                       'sudo systemctl stop chart_viewer.service',
-                       'sudo systemctl restart chart_viewer.service',
-                       'sudo systemctl disable --now chart_viewer.service',
-                       'sudo systemctl enable --now chart_viewer.service',
-                       'sudo systemctl stop spectrogram_viewer.service',
-                       'sudo systemctl restart spectrogram_viewer.service',
-                       'sudo systemctl disable --now spectrogram_viewer.service',
-                       'sudo systemctl enable --now spectrogram_viewer.service',
-                       'sudo systemctl enable '.service_mount().' && sudo reboot',
-                       'sudo systemctl disable '.service_mount().' && sudo reboot',
-                       'stop_core_services.sh',
-                       'restart_services.sh',
-                       'sudo reboot',
-                       'update_birdnet.sh',
-                       'sudo shutdown now',
-                       'sudo clear_all_data.sh');
-      $command = $_GET['submit'];
-    if($submittedpwd == $caddypwd && $submitteduser == 'birdnet' && in_array($command,$allowedCommands)){
+  ensure_authenticated();
+  $allowedCommands = array('sudo systemctl stop livestream.service && sudo systemctl stop icecast2.service',
+                     'sudo systemctl restart livestream.service && sudo systemctl restart icecast2.service',
+                     'sudo systemctl disable --now livestream.service && sudo systemctl disable icecast2 && sudo systemctl stop icecast2.service',
+                     'sudo systemctl enable icecast2 && sudo systemctl start icecast2.service && sudo systemctl enable --now livestream.service',
+                     'sudo systemctl stop web_terminal.service',
+                     'sudo systemctl restart web_terminal.service',
+                     'sudo systemctl disable --now web_terminal.service',
+                     'sudo systemctl enable --now web_terminal.service',
+                     'sudo systemctl stop birdnet_log.service',
+                     'sudo systemctl restart birdnet_log.service',
+                     'sudo systemctl disable --now birdnet_log.service',
+                     'sudo systemctl enable --now birdnet_log.service',
+                     'sudo systemctl stop birdnet_analysis.service',
+                     'sudo systemctl restart birdnet_analysis.service',
+                     'sudo systemctl disable --now birdnet_analysis.service',
+                     'sudo systemctl enable --now birdnet_analysis.service',
+                     'sudo systemctl stop birdnet_stats.service',
+                     'sudo systemctl restart birdnet_stats.service',
+                     'sudo systemctl disable --now birdnet_stats.service',
+                     'sudo systemctl enable --now birdnet_stats.service',
+                     'sudo systemctl stop birdnet_recording.service',
+                     'sudo systemctl restart birdnet_recording.service',
+                     'sudo systemctl disable --now birdnet_recording.service',
+                     'sudo systemctl enable --now birdnet_recording.service',
+                     'sudo systemctl stop chart_viewer.service',
+                     'sudo systemctl restart chart_viewer.service',
+                     'sudo systemctl disable --now chart_viewer.service',
+                     'sudo systemctl enable --now chart_viewer.service',
+                     'sudo systemctl stop spectrogram_viewer.service',
+                     'sudo systemctl restart spectrogram_viewer.service',
+                     'sudo systemctl disable --now spectrogram_viewer.service',
+                     'sudo systemctl enable --now spectrogram_viewer.service',
+                     'sudo systemctl enable '.get_service_mount_name().' && sudo reboot',
+                     'sudo systemctl disable '.get_service_mount_name().' && sudo reboot',
+                     'stop_core_services.sh',
+                     'restart_services.sh',
+                     'sudo reboot',
+                     'update_birdnet.sh',
+                     'sudo shutdown now',
+                     'sudo clear_all_data.sh');
+    $command = $_GET['submit'];
+    if(in_array($command,$allowedCommands)){
       if(isset($command)){
         $initcommand = $command;
 		  if (strpos($command, "systemctl") !== false) {
@@ -348,7 +291,7 @@ if(isset($_GET['view'])){
           $command .= " & sleep 3;sudo systemctl status " . $service_names;
         }
         if($initcommand == "update_birdnet.sh") {
-          unset($_SESSION['behind']);
+          session_unset();
         }
         $results = shell_exec("$command 2>&1");
         $results = str_replace("FAILURE", "<span style='color:red'>FAILURE</span>", $results);
@@ -401,14 +344,8 @@ if(isset($_GET['view'])){
           $results = "This command has no output.";
         }
         echo "<table style='min-width:70%;'><tr class='relative'><th>Output of command:`".$initcommand."`<button class='copyimage' style='right:40px' onclick='copyOutput(this);'>Copy</button></th></tr><tr><td style='padding-left: 0px;padding-right: 0px;padding-bottom: 0px;padding-top: 0px;'><pre class='bash' style='text-align:left;margin:0px'>$results</pre></td></tr></table>"; 
-      } else {
-        header('WWW-Authenticate: Basic realm="My Realm"');
-        header('HTTP/1.0 401 Unauthorized');
-        echo 'You cannot access the web terminal';
-        exit;
       }
     }
-  }
   ob_end_flush();
 } else {include('overview.php');}
 ?>
