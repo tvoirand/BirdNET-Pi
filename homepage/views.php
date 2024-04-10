a22c7c089fa94eb56df04d2b5812d28ef379bba4
frederik <frederik.slos@gmail.com>
2024-04-10 13:04:08 +0200

    fix topnav formatting

--- homepage/views.php
+++ homepage/views.php
@@ -55,36 +55,20 @@ elseif ($config["LONGITUDE"] == "0.000") {
   <link rel="stylesheet" href="style.css?v=<?php echo date ('n.d.y', filemtime('style.css')); ?>">
 </head>
 <body>
+<form action="views.php" method="GET" id="views">
 <div class="topnav" id="myTopnav">
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Overview" form="views">Overview</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Todays Detections" form="views">Today's Detections</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Spectrogram" form="views">Spectrogram</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Species Stats" form="views">Best Recordings</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Streamlit" form="views">Species Stats</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Daily Charts" form="views">Daily Charts</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Recordings" form="views">Recordings</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="View Log" form="views">View Log</button>
-</form>
-<form action="views.php" method="GET" id="views">
   <button type="submit" name="view" value="Tools" form="views">Tools<?php if(isset($_SESSION['behind']) && intval($_SESSION['behind']) >= 50 && ($config['SILENCE_UPDATE_INDICATOR'] != 1)){ $updatediv = ' <div class="updatenumber">'.$_SESSION["behind"].'</div>'; } else { $updatediv = ""; } echo $updatediv; ?></button>
-</form>
-<button href="javascript:void(0);" class="icon" onclick="myFunction()"><img src="images/menu.png"></button>
+  <button type="button" href="javascript:void(0);" class="icon" onclick="myFunction()"><img src="images/menu.png"></button>
 </div>
+</form>
 
 <script>
 window.onload = function() {
