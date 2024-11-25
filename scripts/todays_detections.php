abcbcb601871f99c40ce757010a0772d767653be
Alexandre <44178713+alexbelgium@users.noreply.github.com>
2024-11-25 17:40:10 +0100

    Update scripts/todays_detections.php
    Co-authored-by: Nachtzuster <Nachtzuster@users.noreply.github.com>

--- scripts/todays_detections.php
+++ scripts/todays_detections.php
@@ -191,7 +191,7 @@ if(isset($_GET['ajax_detections']) && $_GET['ajax_detections'] == "true"  ) {
     $iterations++;
 
     $comname = preg_replace('/ /', '_', $todaytable['Com_Name']);
-    $comnamegraph = preg_replace('/\'/', '__', $comname);
+    $comnamegraph = str_replace("'", "\'", $todaytable['Com_Name']);
     $comname = preg_replace('/\'/', '', $comname);
     $filename = "/By_Date/".date('Y-m-d')."/".$comname."/".$todaytable['File_Name'];
     $filename_formatted = $todaytable['Date']."/".$comname."/".$todaytable['File_Name'];
