7fe9a6fe3aeaa350ff1fc076e7e76f99fc143446
frederik <frederik.slos@gmail.com>
2025-01-03 11:22:10 +0100

    make diagnosing recording issues easier: add the recording service log to the birdnet log

--- scripts/birdnet_log.sh
+++ scripts/birdnet_log.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-journalctl --no-hostname -q -o short -fu birdnet_analysis | sed "s/$(date "+%b %d ")//g;s/${HOME//\//\\/}\///g;/Line/d;/find/d;/systemd/d;s/ .*\[.*\]: /---/"
+journalctl --no-hostname -q -o short -fu birdnet_analysis -u birdnet_recording | sed "s/$(date "+%b %d ")//g;s/${HOME//\//\\/}\///g;/Line/d;/find/d;/systemd/d;s/ .*\[.*\]: /---/"
